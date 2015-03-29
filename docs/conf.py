4c5770dd9948c7a4c7f9eb3c8e213eb6bc6855dd
Hong Minhee <hongminhee@member.fsf.org>
2015-03-29 17:48:06 +0900

    Change docs' theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import sass
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.1'
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
@@ -95,7 +95,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
