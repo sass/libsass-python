64cce148a5deb95b83163193deeedcc163bfc427
Anthony Sottile <asottile@umich.edu>
2018-11-13 12:33:54 -0800

    0.16.0

--- sass.py
+++ sass.py
@@ -32,7 +32,7 @@ __all__ = (
     'SassError', 'SassFunction', 'SassList', 'SassMap', 'SassNumber',
     'SassWarning', 'and_join', 'compile', 'libsass_version',
 )
-__version__ = '0.15.1'
+__version__ = '0.16.0'
 libsass_version = _sass.libsass_version
 
 
